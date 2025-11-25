ffe0b8e19174e9456fb77da83f7f70f1991bfd12
janfedyna51-blip <janfedyna51@gmail.com>
2025-11-25 10:23:33 +0000

    Com4_1.1

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // define endpoint for exercise 1 here
-app.get('/math/circle/:r', (req, res) => {
-//TODO1  
+app.get("/math/circle/:r", (req, res) => {
+  //TODO1
+  const radius = parseFloat(req.params.r);
+  const area = Math.PI * radius * radius;
+  const circumference = 2 * Math.PI * radius;
+  const result = {
+    area: area.toFixed(2),
+    circumference: circumference.toFixed(2),
+  };
   res.json(result);
 });
 
 //TODO2
 
-
 //TODO3
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
